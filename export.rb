bedf4f997c1603a409117c444f303b33150533e7
1robface <rperrill@gmail.com>
2017-03-24 11:08:26 -0400

    Update export.rb

--- export.rb
+++ export.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 require 'httparty'
 
 # Configuration
-api_key      = ENV["TUMBLR_API_KEY"]
-username     = ARGV[0] || ENV["TUMBLR_USERNAME"]
+api_key      = ENV["WXRnq7oSKiCqiSmGiSjrhxgj04KW9Fq8xKfRq43VbuYnzaOHcn"]
+username     = ARGV[0] || ENV["redroborsomething"]
 image_dir    = ARGV[1] || "images"
 offset       = 0
 limit        = 20  # number of posts requested each time
